f33e38552dfd769e1ed1befebd6cb446e61ee06d
Jonthss <jonathas9000@gmail.com>
2025-05-15 12:33:45 -0300

    Add initial tents data JSON file with multiple tent entries and details

--- vite.config.js
+++ vite.config.js
@@ -1,67 +1,18 @@
-const path = require('path');
 import { resolve } from "path";
 import { defineConfig } from "vite";
-const fs = require('fs');
-import VitePluginCopy from "vite-plugin-copy";
 
 export default defineConfig({
-  root: 'src/',
+  root: "src/",
 
   build: {
-    outDir: '../dist',
+    outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
-        cart: resolve(__dirname, 'src/cart/index.html'),
-        checkout: resolve(__dirname, 'src/checkout/index.html'),
-        product1: resolve(__dirname, 'src/product_pages/cedar-ridge-rimrock-2.html'),
-        product2: resolve(__dirname, 'src/product_pages/marmot-ajax-3.html'),
-        product3: resolve(__dirname, 'src/product_pages/northface-alpine-3.html'),
-        product4: resolve(__dirname, 'src/product_pages/northface-talus-4.html'),
-        productPagesIndex: resolve(__dirname, 'src/product_pages/index.html'),
+        main: resolve(__dirname, "src/index.html"),
+        cart: resolve(__dirname, "src/cart/index.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
+        product: resolve(__dirname, "src/product_pages/index.html"),
       },
     },
   },
-
-  plugins: [
-    {
-      name: 'copy-js-files', 
-      async closeBundle() {
-        const srcDir = path.resolve(__dirname, 'src/js');  
-        const destDir = path.resolve(__dirname, 'dist/js');
-        
-        const srcJsonDir = path.resolve(__dirname, 'src/json');
-        const destJsonDir = path.resolve(__dirname, 'dist/json');
-        
-        // Cria o diretório de destino se não existir
-        if (!fs.existsSync(destDir)) {
-          fs.mkdirSync(destDir, { recursive: true });
-        }
-
-        if (!fs.existsSync(destJsonDir)) {
-          fs.mkdirSync(destJsonDir, { recursive: true });
-        }
-
-        // Lê todos os arquivos na pasta src/js
-        const files = fs.readdirSync(srcDir);
-
-        const filesJson = fs.readdirSync(srcJsonDir);
-
-        // Copia cada arquivo para a pasta dist/js
-        filesJson.forEach((file) => {
-          const srcFilePathJson = path.join(srcJsonDir, file);
-          const destFilePathJson = path.join(destJsonDir, file);
-          fs.copyFileSync(srcFilePathJson, destFilePathJson); 
-        });
-
-        files.forEach((file) => {
-          const srcFilePath = path.join(srcDir, file);
-          const destFilePath = path.join(destDir, file);
-          fs.copyFileSync(srcFilePath, destFilePath); 
-        });
-
-        
-      },
-    },
-  ],
 });
