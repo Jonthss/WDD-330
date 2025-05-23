68e93499fd8c25d3d4876508a39c96565f65ff83
Jonthss <jonathas9000@gmail.com>
2025-05-22 21:57:28 -0300

    Refactor product page for dynamic loading and improve accessibility
    - Updated index.html to use dynamic loading for product details with placeholders.
    - Added new discount.css for styling discounted prices.
    - Created Alert.js to fetch and display alerts from a JSON file.
    - Added footer and header partials for consistent layout across pages.
    - Introduced alerts.json for managing alert messages and styles.
    - Updated SVG icons in header and footer for better visual representation.

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,8 +1,12 @@
+import { loadHeaderFooter, getParam } from "./utils.mjs";
 import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
 
-const dataSource = new ProductData('tents');
+loadHeaderFooter();
+
+const category = getParam("category");
+const dataSource = new ProductData();
 const listElement = document.querySelector('.product-list');
-const productList = new ProductList('tents', dataSource, listElement);
+const productList = new ProductList(category, dataSource, listElement);
 
 productList.init();
