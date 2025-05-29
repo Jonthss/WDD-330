5739f4fa4116e6d769f0020be575151a104215ee
Jonthss <jonathas9000@gmail.com>
2025-05-29 11:59:45 -0300

    Team Activity W04

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,8 @@
 import { getParam, loadHeaderFooter, updateCartNum } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData("tents");
+const dataSource = new ExternalServices();
 const productID = getParam("product");
 
 const product = new ProductDetails(productID, dataSource);
